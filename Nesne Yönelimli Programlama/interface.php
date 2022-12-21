0c6ce4713d2f5bb1dd92604e89d01d9d0ea7d9f6
mturan01 <mehmetculha96@gmail.com>
2022-12-21 13:37:01 +0300

    interface

---
+++ Nesne Yönelimli Programlama/interface.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Interface Kullanımı
+Arayüzler, soyut sınıflara benzer.
+
+Arayüzler sadece sınıfta zorunlu olarak tanımlanması gereken metotları belirtmek için kullanılır.
+
+Özellikleri
+
+Soyut methodlar ve sabitler içerebilir.
+Sadece public method tanımı yapılabilir.
+Aynı sınıf sadece birden fazla interface kullanabilir.
+*/
+
+interface A{
+
+    public function a();
+    public function b();
+}
+
+interface B extends A{
+
+    public function c();
+    public function d();
+}
+
+class C implements B{
+
+    public function a()
+
+    {
+
+    }
+
+    public function b()
+
+    {
+
+    }
+
+    public function c()
+    {
+
+    }
+
+    public function d()
+    {
+       
+    }
+}
